34152681d90beda8d8e70f46e86b791d29f1e6f4
Tobias <eisenhuth.tobias@gmail.com>
2025-10-01 15:50:07 +0200

    fixup! separates sender proxy and receiver code

--- src/demos/receiver.py
+++ src/demos/receiver.py
@@ -9,25 +9,23 @@ key_store = {}
 artifact_store = {}
 
 def key_gen(sender_id, secret_id):
-    if sender_id not in key_store:
-        key_store[sender_id] = {}
-        if secret_id not in key_store[sender_id]:
-            key_store[sender_id][secret_id] = {}
 
     secret_key = SecretKey.random()
     public_key = secret_key.public_key()
-    key_store[sender_id][secret_id] = {
+
+    key_store.setdefault(sender_id, {})[secret_id] = {
         "secret_key": secret_key,
         "public_key": public_key
     }
+
     return key_store[sender_id][secret_id]["public_key"]
 
 def request_access(sender_id, secret_id):
     public_key = key_gen(sender_id, secret_id)
     payload = {
-        "receiver": RECEIVER_ID,
+        "receiver_id": RECEIVER_ID,
         "secret_id": secret_id,
-        "receiver_pubkey": bytes(public_key)
+        "receiver_public_key": bytes(public_key)
     }
     msg = encode_msg(REQUEST_ACCESS, payload)
     with socket.create_connection((SENDER_HOST, SENDER_PORT)) as sock:
@@ -39,29 +37,22 @@ def request_access(sender_id, secret_id):
     if msg["action"] == GRANT_ACCESS_RECEIVER:
         sender_id = msg["payload"]["sender_id"]
         secret_id = msg["payload"]["secret_id"]
-        sender_public_key = msg["payload"]["public_key"]
-        verifying_key = msg["payload"]["verifying_key"]
+        sender_public_key_bytes = msg["payload"]["public_key"]
+        verifying_key_bytes = msg["payload"]["verifying_key"]
 
-        if sender_id not in artifact_store:
-            artifact_store[sender_id] = {}
-            if secret_id not in artifact_store[secret_id]:
-                artifact_store[sender_id][secret_id] = {
-                    "sender_public_key": sender_public_key,
-                    "verifying_key": verifying_key
-                }
-
+        artifact_store.setdefault(sender_id, {})[secret_id] = {
+            "sender_public_key": PublicKey.from_bytes(sender_public_key_bytes),
+            "verifying_key": PublicKey.from_bytes(verifying_key_bytes)
+        }
 
         print(f"[Receiver] Access granted to '{secret_id}' from '{sender_id}'.")
-    
 
 def request_and_decrypt(sender_id, secret_id):
-    receiver_sk = key_store[sender_id][secret_id]
-    receiver_pk = receiver_sk.public_key()
-
     payload = {
-        "receiver": RECEIVER_ID,
-        "sender": sender_id,
-        "secret_id": secret_id
+        "receiver_id": RECEIVER_ID,
+        "sender_id": sender_id,
+        "secret_id": secret_id,
+        "receiver_public_key": bytes(key_store[sender_id][secret_id]["public_key"])
     }
     msg = encode_msg(REQUEST_SECRET, payload)
     with socket.create_connection((PROXY_HOST, PROXY_PORT)) as sock:
@@ -84,7 +75,7 @@ def request_and_decrypt(sender_id, secret_id):
             for cfrag in suspicious_cfrags]
 
     plaintext = decrypt_reencrypted(
-        receiver_sk,
+        key_store[sender_id][secret_id]["secret_key"],
         artifact_store[sender_id][secret_id]["sender_public_key"],
         capsule,
         cfrags,
